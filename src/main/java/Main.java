f69c6b15b63a2d8fbdb0f2ff71c6bd0a20ed5184
merafe26MRF <melanyefernandez200@gmail.com>
2024-12-01 17:13:22 -0600

    Creacion Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
-
+import vista.VistaBanco;
+import controlador.ControladorBanco;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        // Creamos la vista
+        VistaBanco vista = new VistaBanco();
+
+        // Crea el controlador, pasando la vista y el saldo inicial de la cuenta
+        new ControladorBanco(vista, 5000.0);
+
+        // creamos  la ventana de la vista
+        vista.setVisible(true);
+
+}
 }
